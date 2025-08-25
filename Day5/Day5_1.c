84c434ed04fe727b3a560f10c67c7723579523c4
nitin-gupta06 <guptanitin7770@gmail.com>
2025-08-25 12:36:59 +0530

    Day5

---
+++ Day5/Day5_1.c
@@ -0,0 +1,41 @@
+/*
+Q9: Write a program to calculate simple and compound interest for given principal, rate, and time.
+
+Sample Test Cases:
+Input 1:
+1000 5 2
+Output 1:
+Simple Interest=100, Compound Interest=102.5
+
+Input 2:
+5000 7 3
+Output 2:
+Simple Interest=1050, Compound Interest=1125.76
+
+*/
+
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+	
+	printf("Program to calculate simple and compound interest for given principal, rate, and time.\n\n");
+	float principal, rate, time, amount, d, compoundInterest, simpleInterest, difference;
+    printf("Enter principal amount: ");
+    scanf("%f", &principal);
+    printf("Enter rate of interest (in percentage): ");
+    scanf("%f", &rate);
+    printf("Enter time (in years): ");
+    scanf("%f", &time);
+
+    // Calculate Simple Interest
+    simpleInterest = (principal * rate * time) / 100;
+    // Calculate Compound Interest
+    d = 1 + (rate / 100);
+    amount = principal * pow(d, time);
+    compoundInterest = amount - principal;
+    
+    printf("Simple Interest: %.2f, Compound Interest: %.2f \n\n", simpleInterest, compoundInterest);
+    
+    return 0;
+}
---
+++ Day5/Day5_2.c
@@ -0,0 +1,37 @@
+/*
+Q10: Write a program to input time in seconds and convert it to hours:minutes:seconds format.
+
+Sample Test Cases:
+Input 1:
+3661
+Output 1:
+1:1:1
+
+Input 2:
+7322
+Output 2:
+2:2:2
+
+*/
+
+#include <stdio.h>
+
+int main() {
+
+	printf("Program to input time in seconds and convert it to hours:minutes:seconds format.\n\n");
+	
+	int timeinSec;
+	printf("Enter time in seconds: ");
+	scanf("%d", &timeinSec);
+	
+	// Convert in format
+	int hours, minutes, seconds;
+	
+	hours = timeinSec / 3600;
+	minutes = (timeinSec % 3600) / 60;
+	seconds = timeinSec % 60;
+	
+	printf("%d seconds= %d:%d:%d\n\n", timeinSec, hours, minutes, seconds);
+	
+	return 0;
+}
